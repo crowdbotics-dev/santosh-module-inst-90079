5ebb1942ecbc9ba2b0406b303ff8fe5a7596046d
Santosh Purbey <santosh.p@crowdbotics.com>
2023-06-09 20:10:49 +0545

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "santosh-module-inst-90079",
   url: "https://santosh-module-inst-90079.botics.co",
   api: "https://santosh-module-inst-90079.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
