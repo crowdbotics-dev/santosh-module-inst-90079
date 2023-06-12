f51615af88fa9b62d65352124a80ab7591a21a91
Santosh Purbey <santosh.p@crowdbotics.com>
2023-06-12 19:43:17 +0545

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "santosh-module-inst-90079",
-  url: "https://santosh-module-inst-90079.botics.co",
-  api: "https://santosh-module-inst-90079.botics.co/api/v1"
+  "name": "santosh-module-inst-90079",
+  "url": "https://santosh-module-inst-90079.botics.co",
+  "api": "https://santosh-module-inst-90079.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
